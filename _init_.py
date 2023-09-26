ae486ccb42a0ce867d6b860bc4829dec1504dafd
Tyler Carnemolla <tymolla53@gmail.com>
2023-09-26 11:05:32 -0700

    first commit

---
+++ _init_.py
@@ -0,0 +1,7 @@
+from math import pi as p
+print(p)
+def find_circ(radius):
+    circumference=2 * p * radius
+    print(circumference)
+
+find_circ()
---
+++ new_mods.py
@@ -0,0 +1,10 @@
+# square footage calculator
+
+def square_feet(length,width):
+    answer=length*width
+    print(answer)
+
+import math
+from math import pi as p
+
+
